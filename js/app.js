ca070de0009c38bb1800d3dfc2e55f762da2a830
Madhumitha <madhumithavenu03@gmail.com>
2023-07-31 11:11:19 +0530

    Displaying the Portfolio

--- js/app.js
+++ js/app.js
@@ -9,6 +9,20 @@ function loadPortfolio(){
     fetch (url)
     .then(response => response.json())
     .then(data => {
-        console.log(data.portfolio);
+        // console.log(data.portfolio);
+        let html = '';
+        data.portfolio.forEach(item => {
+            html += `
+                <div class="item">
+                    <img src="img/${item.id}.jpg">
+                    <div class="content">
+                    <h3>${item.projectname}</h3>
+                    <p>${item.desc}</p>
+                    </div>
+                </div>
+            `;
+        });
+        // console.log(html);
+        document.querySelector('#portfolio').innerHTML = html;
     })
 }
